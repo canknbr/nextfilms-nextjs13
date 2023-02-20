36f11544c4c193d634a49c101d09ed54f50f2a7c
Can Kanbur <cankanbur1@gmail.com>
2023-02-20 15:24:14 +0300

    added icons package

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,26 @@
+import { Inter } from '@next/font/google';
+import '@/styles/reset.css';
+import '@/styles/global.css';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
-import '@/styles/reset.css'
-import '@/styles/global.css'
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className="container">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
