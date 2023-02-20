faa5e8b31f851da817c01f11c09d2495ac9ece3a
Can Kanbur <cankanbur1@gmail.com>
2023-02-20 17:58:38 +0300

    added error, loading and not found page

---
+++ app/movie/[id]/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Loading from '@/components/Loading';
+function loading() {
+  return (
+    <Loading />
+  );
+}
+
+
+
+export default loading
--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -2,10 +2,13 @@ import React from 'react'
 import {notFound} from 'next/navigation'
 import MovieContainer from '@/containers/movie';
 import Movies from '../../../mocks/movies.json';
-function MoviePage({params}) {
+function MoviePage({params,searchParams}) {
 const {results} = Movies;
 const movieDetails = results.find((movie) => movie.id.toString() === params.id);
 if(!movieDetails) return notFound();
+if(searchParams.error === 'true') {
+  throw new Error('Error from query param');
+}
 
   return (
     <MovieContainer movie={movieDetails} />
